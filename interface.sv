553ec9d515047c65818b25d552a48d265a920b4a
Karthik16701 <109361156+Karthik16701@users.noreply.github.com>
2023-09-25 16:08:24 +0530

    Update interface.sv

--- interface.sv
+++ interface.sv
@@ -14,21 +14,18 @@ interface fifo_interface(input clk, reset);
     output i_wren;
     output i_rden;
     output i_wrdata;
- //   input o_full;
- //   input o_empty;
- //   input o_alm_full;
- //   input o_alm_empty;
- //   input [DATA_W-1:0] o_rddata;
+    input o_full;
+    input o_empty;
+    input o_alm_full;
+    input o_alm_empty;
+   input [127:0] o_rddata;
   endclocking
   
-  clocking m_cb_in @(posedge clk);
+  clocking m_cb @(posedge clk);
     default input #1 output #1;
     input i_wren;
     input i_rden;
     input i_wrdata;
-endclocking
-clocking m_cb_out @(posedge clk);
-    default input #1 output #1;
     input o_full;
     input o_empty;
     input o_alm_full;
@@ -37,7 +34,7 @@ clocking m_cb_out @(posedge clk);
   endclocking
   
   modport d_mp (input clk, reset, clocking d_cb);
-  modport m_mp_in (input clk, reset, clocking m_cb_in);
-  modport m_mp_out (input clk, reset, clocking m_cb_out);
+  modport m_mp (input clk, reset, clocking m_cb);
+  
     
 endinterface
