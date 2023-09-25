54fa90efcdabdddb177d136ceeab0bf526412a4a
Karthik16701 <109361156+Karthik16701@users.noreply.github.com>
2023-09-25 16:11:42 +0530

    Update environment.sv

--- environment.sv
+++ environment.sv
@@ -3,8 +3,8 @@
 //`include "fifo_scoreboard.sv"
 
 class fifo_environment extends uvm_env;
-  fifo_active_agent fifo_act_agt;
-  fifo_passive_agent fifo_pass_agt;
+  fifo_agent fifo_agt;
+ 
   fifo_scoreboard fifo_scb;
   `uvm_component_utils(fifo_environment)
   
@@ -14,14 +14,12 @@ function new(string name = "fifo_environment", uvm_component parent);
   
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    fifo_act_agt = fifo_active_agent::type_id::create("fifo_act_agt", this);
-    fifo_pass_agt = fifo_passive_agent::type_id::create("fifo_pass_agt", this);
-    fifo_scb = fifo_scoreboard::type_id::create("fifo_scb", this);
+    fifo_agt = fifo_active_agent::type_id::create("fifo_act_agt", this);
+     fifo_scb = fifo_scoreboard::type_id::create("fifo_scb", this);
   endfunction
   
   virtual function void connect_phase(uvm_phase phase);
-    fifo_act_agt.fifo_act_mon.item_got_port.connect(fifo_scb.item_got_export1);
-    fifo_pass_agt.fifo_pass_mon.item_got_port2.connect(fifo_scb.item_got_export2);
-  endfunction
+    fifo_agt.fifo_mon.item_got_port.connect(fifo_scb.item_got_export);
+   endfunction
   
 endclass
