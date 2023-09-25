048e0804380dae9ad5d8da67063b68134d12aa7c
Karthik16701 <109361156+Karthik16701@users.noreply.github.com>
2023-09-25 15:50:59 +0530

    Update active_agent.sv

--- active_agent.sv
+++ active_agent.sv
@@ -5,27 +5,27 @@
 //`include "fifo_active_monitor.sv"
 //`include "fifo_active_agent.sv"
 
-class fifo_active_agent extends uvm_agent;
+class fifo_agent extends uvm_agent;
   fifo_sequencer fifo_seqr;
   fifo_driver fifo_dri;
-  fifo_active_monitor fifo_act_mon;
-  `uvm_component_utils(fifo_active_agent)
+  fifo_monitor fifo_mon;
+  `uvm_component_utils(fifo_agent)
   
-  function new(string name = "fifo_active_agent", uvm_component parent);
+  function new(string name = "fifo_agent", uvm_component parent);
     super.new(name, parent);
   endfunction
   
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
- //  if(get_is_active() == UVM_ACTIVE) begin
+   if(get_is_active() == UVM_ACTIVE) begin
       fifo_seqr = fifo_sequencer::type_id::create("fifo_seqr", this);
       fifo_dri = fifo_driver::type_id::create("fifo_dri", this);
- //  end
-    fifo_act_mon = fifo_active_monitor::type_id::create("fifo_mon", this);
+   end
+    fifo_mon = fifo_monitor::type_id::create("fifo_mon", this);
   endfunction
   
   virtual function void connect_phase(uvm_phase phase);
- //   if(get_is_active() == UVM_ACTIVE)
+    if(get_is_active() == UVM_ACTIVE)
       fifo_dri.seq_item_port.connect(fifo_seqr.seq_item_export);
   endfunction
   
