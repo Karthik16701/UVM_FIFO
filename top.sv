654849b3fbf66c3922bebf25415fb2ec8aab141e
Karthik16701 <109361156+Karthik16701@users.noreply.github.com>
2023-09-27 10:49:04 +0530

    Update top.sv

--- top.sv
+++ top.sv
@@ -1,3 +1,5 @@
+ // Code your testbench here
+// or browse Examples
 import uvm_pkg::*;
 `include "uvm_macros.svh"
 `include "interface.sv"
@@ -6,10 +8,11 @@ import uvm_pkg::*;
 `include "sequencer.sv"
 `include "driver.sv"
 `include "monitor.sv"
-`include " agent.sv"
+`include "agent.sv"
 `include "scoreboard.sv"
 `include "environment.sv"
 `include "test.sv"
+`include "design.sv"
 
 module tb;
   bit clk;
@@ -27,15 +30,15 @@ module tb;
   fifo_interface tif(clk, reset);
   
   my_fifo dut(.clk(tif.clk),
-               .rst(tif.reset),
-               .data_in(tif.i_wrdata),
-               .write_en(tif.i_wren),
-               .read_en(tif.i_rden),
-               .full(tif.o_full),
-               .empty(tif.o_empty),
-               .almost_full(tif.o_alm_full),
-               .almost_empty(tif.o_alm_empty),
-               .data_out(tif.o_rddata));
+               .rstn(tif.reset),
+               .i_wrdata(tif.i_wrdata),
+               .i_wren(tif.i_wren),
+               .i_rden(tif.i_rden),
+               .o_full(tif.o_full),
+               .o_empty(tif.o_empty),
+               .o_alm_full(tif.o_alm_full),
+               .o_alm_empty(tif.o_alm_empty),
+               .o_rddata(tif.o_rddata));
   
   initial begin
    uvm_config_db#(virtual fifo_interface)::set(null, "", "vif", tif);
