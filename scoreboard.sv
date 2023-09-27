4bf756b3a86695b9386c5297726d9eacaa1f9fb2
Karthik16701 <109361156+Karthik16701@users.noreply.github.com>
2023-09-27 10:46:01 +0530

    Update scoreboard.sv

--- scoreboard.sv
+++ scoreboard.sv
@@ -1,14 +1,10 @@
- // Code your testbench here
-// or browse Examples
  class fifo_scoreboard extends uvm_scoreboard;
-   uvm_analysis_imp#(fifo_sequence_item, fifo_scoreboard) item_got_export;
- 
+  uvm_analysis_imp#(fifo_sequence_item, fifo_scoreboard) item_got_export;
   `uvm_component_utils(fifo_scoreboard)
   
-   function new(string name = "fifo_scoreboard", uvm_component parent);
+  function new(string name = "fifo_scoreboard", uvm_component parent);
     super.new(name, parent);
-     item_got_export1 = new("item_got_export1", this);
- 
+    item_got_export = new("item_got_export", this);
   endfunction
   
   virtual function void build_phase(uvm_phase phase);
@@ -16,33 +12,28 @@
   endfunction
   
   bit [127:0] queue[$];
- 
   
-   function void write(input fifo_sequence_item item_got);
-     bit [127:0] examdata;
-     if(item_got.i_wren == 'b1)
-       begin
-         if(queue.size()<1024)
-           begin
-       queue.push_back(item_got.i_wrdata);
-     
-             `uvm_info("write Data", $sformatf("i_wren: %0b i_rden: %0b i_wrdata: %0d full: %0b",item_got.i_wren, item_got.i_rden,item_got.i_wrdata), UVM_LOW);
+  function void write(input fifo_sequence_item item_got);
+    bit [127:0] examdata;
+    if(item_got.i_wren == 'b1)begin
+      queue.push_back(item_got.i_wrdata);
+      `uvm_info("write Data", $sformatf("wr: %0b rd: %0b data_in: %0h full: %0b",item_got.i_wren, item_got.i_rden,item_got.i_wrdata, item_got.o_full), UVM_LOW);
     end
-      if (item_got.i_rden == 'b1)
-        begin
-      if(queue.size() >= 'd1)
-        begin     
+    else if (item_got.i_rden == 'b1)begin
+      if(queue.size() >= 'd1)begin
         examdata = queue.pop_front();
-          `uvm_info("Data read operation", $sformatf("data: %0d , the read data is(local var)=%0d", data,temp_var), UVM_LOW);
-          if(examdata == temp_var)
+        `uvm_info("Read Data", $sformatf("examdata: %0h data_out: %0h empty: %0b", examdata, item_got.o_rddata, item_got.o_empty), UVM_LOW);
+        if(examdata == item_got.o_rddata)
+          begin
           $display("-------- 		Pass! 		--------");
-        
-        else 
+        end
+        else begin
           $display("--------		Fail!		--------");
+          $display("--------		Check empty	--------");
+        end
       end
     end
   endfunction
 endclass
-         
          
          
