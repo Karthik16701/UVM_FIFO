fcf563ac05d487032343d167e3bcdfe2ab6fe2ea
Karthik16701 <109361156+Karthik16701@users.noreply.github.com>
2023-09-27 10:43:57 +0530

    Update sequence.sv

--- sequence.sv
+++ sequence.sv
@@ -6,27 +6,27 @@
   endfunction
   
   virtual task body();
-    repeat(1024) begin
+    repeat(10) begin
       req = fifo_sequence_item::type_id::create("req");
       start_item(req);
       assert(req.randomize() with {i_wren == 1;i_rden ==0;});
       finish_item(req);
     end
     
-    repeat(1024) begin
+    repeat(10) begin
       req = fifo_sequence_item::type_id::create("req");
       start_item(req);
       assert(req.randomize() with {i_wren==0;i_rden == 1;});
       finish_item(req);
     end
  
-     repeat(1024) begin
-      req = fifo_sequence_item::type_id::create("req");
-      start_item(req);
-       assert(req.randomize() with {i_wren==0;i_rden == 1;});
-      assert(req.randomize() with {i_wren == 1;i_rden ==0;});
-      finish_item(req);
-    end
+//      repeat(10) begin
+//       req = fifo_sequence_item::type_id::create("req");
+//       start_item(req);
+//        assert(req.randomize() with {i_wren==0;i_rden == 1;});
+//       assert(req.randomize() with {i_wren == 1;i_rden ==0;});
+//       finish_item(req);
+//     end
     
   endtask
   
