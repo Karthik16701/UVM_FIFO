20c6b570d68eb07f9ec21ba30ab8b0c1465dc382
Karthik16701 <109361156+Karthik16701@users.noreply.github.com>
2023-09-27 12:49:27 +0530

    Update scoreboard.sv

--- scoreboard.sv
+++ scoreboard.sv
@@ -1,4 +1,4 @@
- class fifo_scoreboard extends uvm_scoreboard;
+  class fifo_scoreboard extends uvm_scoreboard;
   uvm_analysis_imp#(fifo_sequence_item, fifo_scoreboard) item_got_export;
   `uvm_component_utils(fifo_scoreboard)
   
@@ -12,14 +12,17 @@
   endfunction
   
   bit [127:0] queue[$];
-  
+      bit [127:0] examdata;
+
   function void write(input fifo_sequence_item item_got);
-    bit [127:0] examdata;
-    if(item_got.i_wren == 'b1)begin
+    if(item_got.i_wren == 'b1)
+      begin
       queue.push_back(item_got.i_wrdata);
       `uvm_info("write Data", $sformatf("wr: %0b rd: %0b data_in: %0h full: %0b",item_got.i_wren, item_got.i_rden,item_got.i_wrdata, item_got.o_full), UVM_LOW);
     end
-    else if (item_got.i_rden == 'b1)begin
+    
+     if (item_got.i_rden == 'b1)
+       begin
       if(queue.size() >= 'd1)begin
         examdata = queue.pop_front();
         `uvm_info("Read Data", $sformatf("examdata: %0h data_out: %0h empty: %0b", examdata, item_got.o_rddata, item_got.o_empty), UVM_LOW);
